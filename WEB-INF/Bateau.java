e998ef8c236369413140f1ec47605832c1a39def
Fanilo00 <faniloandriambolanoro@gmail.com>
2023-06-14 22:56:32 +0300

    class bateau

--- WEB-INF/Bateau.java
+++ WEB-INF/Bateau.java
@@ -1,3 +1,24 @@
 public class Bateau {
+    int ID_Bateau;
+    String categorie;
+    int Pavillon;
     
+    public void setID_Bateau(int iD_Bateau) {
+        ID_Bateau = iD_Bateau;
+    }
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+    public void setPavillon(int pavillon) {
+        Pavillon = pavillon;
+    }
+    public int getID_Bateau() {
+        return ID_Bateau;
+    }
+    public String getCategorie() {
+        return categorie;
+    }
+    public int getPavillon() {
+        return Pavillon;
+    }
 }
